a0f2cc160141288166e1158f6c72978d75d04a5e
Pau Cervera i Badia <pau.cervera@biteright.dental>
2024-03-24 17:11:27 +0100

    Patched dependency to pass the test

---
+++ README.md
@@ -0,0 +1,7 @@
+# dipython
+
+This repo if for exploring depencency injection in python, as of now, it
+accompaines the blog post: [DI in
+python](https://self.paudirac.com/blog/di-in-python/).
+
+Use at your own risk.
--- test_lister.py
+++ test_lister.py
@@ -1,8 +1,15 @@
+from unittest import mock
 from lister import MovieLister
-
+from movies import Movie
 
 def test_lister():
-    lister = MovieLister()
-    spielberg_movies = lister.movies_directed_by('Spielberg')
-    assert len(spielberg_movies) > 0
-    assert all(movie.director == 'Spielberg' for movie in spielberg_movies)
+    spielberg = [
+        Movie(year=1979, name="1941", director="Spielberg"),
+    ]
+    with mock.patch('lister.MovieFinder') as mocked_finder_class:
+        mocked_finder_class.return_value.find_all.return_value = spielberg
+        movie_lister = MovieLister()
+        spielberg_movies = movie_lister.movies_directed_by('Spielberg')
+        mocked_finder_class.return_value.find_all.assert_called_once()
+        assert len(spielberg_movies) > 0
+        assert all(movie.director == 'Spielberg' for movie in spielberg_movies)
