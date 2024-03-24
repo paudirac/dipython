2bfc730ba8ecca6446c1069fa65a44a63b477c84
Pau Cervera i Badia <pau.cervera@biteright.dental>
2024-03-24 17:44:22 +0100

    Add typing to see the abstractions

--- lister.py
+++ lister.py
@@ -1,3 +1,6 @@
+import typing
+
+from movies import Movie
 from finder import MovieFinder
 
 
@@ -6,6 +9,6 @@ class MovieLister:
     def __init__(self):
         self.finder = MovieFinder()
 
-    def movies_directed_by(self, director):
+    def movies_directed_by(self, director: str) -> typing.List[Movie]:
         all_movies = self.finder.find_all()
         return [movie for movie in all_movies if movie.director == director]
